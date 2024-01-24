35b63262ef21d12db3cf4c09e8f91558fe756054
Ferdous <hassanferdous101@gmail.com>
2024-01-24 11:52:11 +0600

    added berge visularizer

--- app/page.js
+++ app/page.js
@@ -1,113 +1,112 @@
+"use client";
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [pathId, setPathId] = useState(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const hanldeMouseover = (e) => {
+		const pathId = e.target.dataset.code;
+		setPathId(pathId);
+	};
+	const hanldeMouseLeave = (e) => {
+		setPathId(null);
+	};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleClick = (e) => {
+		var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		const target = e.target;
+		target.style.fill = `#${randomColor}`;
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="relative inline-block">
+				<svg width={451} height={357}>
+					<g transform="scale(0.877431906614786) translate(0, 0.9345898004434668)">
+						<path
+							d="M208.55 95.04L209.27 280.72L383.94 322.05L440.13 334.42L451.32 337.03L470.90 341.80L473.56 342.71L475.16 62.48"
+							data-code="id1"
+							fill="#812e40"
+							fillOpacity={1}
+							stroke={pathId === "id1" ? "blue" : "none"}
+							strokeWidth={pathId === "id1" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+						<path
+							d="M157.46 102.39L157.50 100.31L170.90 97.88L204.57 93.31L208.22 94.53L209.00 280.67L194.08 277.17L170.74 275.47L157.50 273.67"
+							data-code="id2"
+							fill="#ddd6c6"
+							fillOpacity={1}
+							stroke={pathId === "id2" ? "blue" : "none"}
+							strokeWidth={pathId === "id2" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+						<path
+							d="M0.00 65.75L156.60 100.03L157.17 297.66L0.00 372.18"
+							data-code="id3"
+							fill="#812e40"
+							fillOpacity={1}
+							stroke={pathId === "id3" ? "blue" : "none"}
+							strokeWidth={pathId === "id3" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+						<path
+							d="M514.00 57.55L211.57 94.43L206.70 92.60L173.23 96.86L157.31 99.75L0.00 65.25L0.00 0.00L514.00 0.00"
+							data-code="id4"
+							fill="#ddd6c6"
+							fillOpacity={1}
+							stroke={pathId === "id4" ? "blue" : "none"}
+							strokeWidth={pathId === "id4" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+						<path
+							d="M475.40 62.48L474.00 354.92L476.37 362.98L507.92 368.69L514.00 374.75L514.00 57.86"
+							data-code="id5"
+							fill="#ddd6c6"
+							fillOpacity={1}
+							stroke={pathId === "id5" ? "blue" : "none"}
+							strokeWidth={pathId === "id5" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+						<path
+							d="m0.5,352.453125c0,0 154,-80 155,-80c1,0 298,87 300,86c2,-1 11,-15 12,-15c1,0 49,12 48.5,11.546875c0.5,0.453125 -1.5,53.453125 -2,53c0.5,0.453125 -515.5,0.453125 -515,0c-0.5,0.453125 1.5,-55.546875 1.5,-55.546875z"
+							data-code="id6"
+							fill="#f9f6e1"
+							fillOpacity={1}
+							stroke={pathId === "id6" ? "blue" : "none"}
+							strokeWidth={pathId === "id6" ? 1 : "none"}
+							strokeOpacity={1}
+							fillRule="evenodd"
+							onMouseOver={hanldeMouseover}
+							onMouseLeave={hanldeMouseLeave}
+							onClick={handleClick}
+						/>
+					</g>
+					<g />
+				</svg>
+				<Image className="pointer-events-none" fill src="/assets/overlay.png" alt="overlay" />
+			</div>
+		</main>
+	);
 }
